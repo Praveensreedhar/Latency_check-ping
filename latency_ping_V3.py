b7d5e5cdd3c302f039346cad52ef48419b2a6826
Praveen <49310101+Praveensreedhar@users.noreply.github.com>
2023-03-01 14:57:34 +0530

    Influx DB Connect and update code

---
+++ latency_ping_V3.py
@@ -0,0 +1,41 @@
+import subprocess
+from influxdb import InfluxDBClient
+import influxdb
+import subprocess, sys
+import os
+import re
+
+os.chdir("/AUTOMATION/GW-LATENCY-CHECK/")
+gateways = open('gateway.txt')
+
+for gateway in gateways:
+    host = gateway.strip()
+    #host = "10.10.1.1"
+    ping_count = 2  # Number of pings to send
+    ping_command = ["ping", "-c", str(ping_count), host]
+    ping_output = subprocess.Popen(ping_command, stdout=subprocess.PIPE).stdout.read().decode()
+
+
+    # Extract the average round-trip time from the output
+    rtt_pattern = r"min/avg/max/mdev = \d+\.\d+/(\d+\.\d+)/\d+\.\d+/\d+\.\d+"
+    rtt_match = re.search(rtt_pattern, ping_output)
+
+    if rtt_match:
+        rtt = float(rtt_match.group(1))
+        print(f"Average RTT to {host}: {rtt} ms")
+    else:
+        print(f"Failed to get RTT for {host}")
+    try:
+
+        connect = InfluxDBClient(host='localhost',
+                               port=8086)
+                                #username='admin',
+                                #password='password'
+        line = "Latency,domain=" + str(host) + " latency=" + str(rtt)
+        #line = "Latency,domain=" + str(url) + " latency=" + str(latencyd[0]).strip('[]')
+        #line = "Latency,domain=" + str(url) + " latency=" + str(latencyd[0])
+        print(line)
+        connect.write([line], {'db': 'GWLATENCYCHECK'}, 204, 'line')
+        connect.close()
+    except influxdb.exceptions.InfluxDBClientError:
+        print("No data")
